081e78799441f7beeb2f92e9ebdf4821381ca8ca
Lars <Lars@lessing.systems>
2016-04-20 01:21:57 +0200

    removed unnecessary lines from main.c

--- src/main.c
+++ src/main.c
@@ -4,11 +4,8 @@
 
 int main()
 {
-        int charcount = strlen(HEXSTRING);
-    	int bitcount = charcount * 4;
         printf("Hexadecimal to convert to Base64:\n");
 	puts( HEXSTRING );
-    	printf("\nLength (bits/chars/bytes): %i/%i/%i", bitcount, charcount, bitcount/8);
     	printf("\nSolution 1:\n");
         char *b64HS = (char*) calloc(b64length(HEXSTRING), 1);
         hexstring_encode(b64HS, HEXSTRING, strlen(HEXSTRING));
