3c5bfd2f962b81af0a7200ee97c026339bc9994d
0rbiter <lessing86@googlemail.com>
2016-04-20 17:08:57 +0200

    added comments

--- src/main.c
+++ src/main.c
@@ -2,8 +2,15 @@
 #include "cryptodata.h"
 #include "cryptodef.h"
 
+char *HEXSTRING3 = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
 int main()
 {
+        /*
+         * convert from string to hex/bytes and Base 64 decode it
+         * */
+        char *HEXSTRING = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
         printf("Hexadecimal to convert to Base64:\n");
 	puts( HEXSTRING );
     	printf("\nSolution 1:\n");
@@ -18,7 +25,13 @@ int main()
 
         free(decoded_string);
         free(b64HS);
-        
+
+        /*
+         * Challenge 2, XOR 2 HEX values from input strings + Base64 decode
+         */
+        char *HEXSTRING1 = "1c0111001f010100061a024b53535009181c";
+        char *HEXSTRING2 = "686974207468652062756C6C277320657965";
+
         puts("XOR these two hex strings:");
         puts(HEXSTRING1);
         puts(HEXSTRING2);
