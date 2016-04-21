20d650a5346508f452ad1817aa40dd0b34ccf150
0rbiter <lessing86@googlemail.com>
2016-04-21 12:20:44 +0200

    Added files, fixed display of results; next: refactor letter scoring into scoring.h

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 
 void separate()
 {
-        puts("\n\n###############################################################");
+        puts("\n\n###############################################################################");
 }
 
 int main()
@@ -14,9 +14,9 @@ int main()
          * */
         char *HEXSTRING = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
-        printf("Hexadecimal to convert to Base64:\n");
-	puts( HEXSTRING );
-    	printf("\nSolution 1:\n");
+        printf("\nChallenge 1\nHexadecimal to convert to Base64:\n");
+	puts(HEXSTRING);
+    	printf("\nSolution:\n");
         char *b64HS = (char*) calloc(b64length(HEXSTRING), 1);
         hexstring_encode_b64(b64HS, HEXSTRING, strlen(HEXSTRING));
         puts(b64HS);
@@ -35,13 +35,14 @@ int main()
          */
         char *HEXSTRING1 = "1c0111001f010100061a024b53535009181c";
         char *HEXSTRING2 = "686974207468652062756C6C277320657965";
-        puts("XOR these two hex strings:");
+        puts("Challenge 2\nXOR these two hex strings & base64 decode it:");
         puts(HEXSTRING1);
         puts(HEXSTRING2);
+        puts("\nXOR'd:");
         char *xor_b64 = calloc(b64length(HEXSTRING1), 1);
         xor_hexstrings(xor_b64, HEXSTRING1, HEXSTRING2);
         puts(xor_b64);
-  
+        puts("\nBase64 decoded:");
         char *xor_string = calloc(strlen(xor_b64), 1);
         b64_decode_string(xor_string, xor_b64, strlen(xor_b64));
         puts(xor_string);
@@ -57,6 +58,8 @@ int main()
         char *STRING3 = calloc(strlen(HEXSTRING3)/2+1, 1);
         char SINGLECHAR[] = "A\0";
         int i, c;
+        printf("Challenge 3\nfind the single byte to decrypt:\n");
+        puts(HEXSTRING3);
         
         hexstringToString(STRING3, HEXSTRING3);
        
@@ -116,14 +119,14 @@ int main()
                         }
                 }
                 points += letters_counted/5;
-                if(points > 200) {
+                if(points > 212) {
                         puts(NEWSTRING);
                         for(l=0; l < 26; l++) {
                                 if(l % 6 == 0 && l != 0)
                                         printf("\n");
                                 printf("%c %.2f\t", A+l, myscores[l]);
                         }
-                        printf("\nPoints gained: %li \t\tLetters counted: %li\n\n", points, letters_counted);
+                        printf("\nPoints gained: %li \t\tLetters counted: %li\t\tKey: %c\n", points, letters_counted, i);
                 }
               
         }
@@ -138,19 +141,23 @@ int main()
         /*
          * Challenge 5
          */
-        puts("XOR these two strings");
+        printf("Challenge 5\nXOR this text with the 3 character key:\n");
         char *STRING4 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
-        char *STRING5 = "ICE";
-        puts("XOR these two hex strings:");
         puts(STRING4);
+        printf("\n");
+
+        char *STRING5 = "ICE";
+        puts("Key:");
         puts(STRING5);
+        printf("\n");
 
         char *xor_string2 = calloc(strlen(STRING4)+1, 1);
         char *xor_hexstring = calloc(strlen(STRING4)*2+1, 1);
         xor_strings(xor_string2, STRING4, STRING5);
         bitsToHexchar(xor_hexstring, xor_string2);
-
+        puts("Solution:");
         puts(xor_hexstring);
+
         free(xor_hexstring);
         free(xor_string2);
 }
