8b549b7b3c338c58be9872805c38e2e1ff90da0f
0rbiter <lessing86@googlemail.com>
2016-05-03 16:55:42 +0200

    added tools

---
+++ include/tools.h
@@ -0,0 +1,7 @@
+/* tools.h */
+#ifndef TOOLS_H
+#define TOOLS_H
+void *xmalloc(size_t size);
+void *xcalloc(size_t items, size_t size);
+void *xrealloc(void *rbuf, size_t size);
+#endif /* !tools.h */
---
+++ src/tools.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *xcalloc(size_t items, size_t size)
+{
+        void *tmp = calloc(items, size);
+        if(tmp != NULL)
+                return tmp;
+        else {
+                fprintf(stderr, "Error: could not allocate memory");
+                printf("Error: could not allocate memory");
+                exit(-1);
+        }
+}
+
+void *xmalloc(size_t size)
+{
+        void *tmp = calloc(size, 1);
+        if(tmp != NULL)
+                return tmp;
+        else {
+                fprintf(stderr, "Error: could not allocate memory");
+                printf("Error: could not allocate memory");
+                exit(-1);
+        }
+}
+
+void *xrealloc(void *rbuf, size_t size)
+{
+        void *tmp = realloc(rbuf, size);
+        if(tmp != NULL) {
+                return tmp;
+        }
+        else {
+                fprintf(stderr, "Error: could not allocate memory");
+                printf("Error: could not allocate memory");
+                exit(-1);
+        }
+
+}
